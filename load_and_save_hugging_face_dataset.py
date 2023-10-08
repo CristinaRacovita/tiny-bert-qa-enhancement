ac954905d8feff80722cd23b749f67f210e05b6e
Cristina Racovita <cristina.racovita98@gmail.com>
2023-10-08 13:53:02 +0200

    # project setup - add dataset in gitignore and implement load and save hugging dataset

---
+++ load_and_save_hugging_face_dataset.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from datasets import load_dataset
+
+# Load the Hugging Face dataset (replace 'dataset_name' with the name of your dataset)
+dataset = load_dataset('squad')
+
+# Convert the dataset to a Pandas DataFrame
+validation = pd.DataFrame(dataset['validation']) 
+train = pd.DataFrame(dataset['train'])
+# Specify the path to save the CSV file
+VALIDATION_CSV_FILE_PATH = 'squad_validation.csv'
+TRAIN_CSV_FILE_PATH = 'squad_validation.csv'
+
+# Save the DataFrame to a CSV file
+validation.to_csv(VALIDATION_CSV_FILE_PATH, index=False)
+train.to_csv(VALIDATION_CSV_FILE_PATH, index=False)
